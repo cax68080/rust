04a0339d3547f86ee7bd492eb01d55071f9b5342
Takeo Yamazaki <cax68080@pop06.odn.ne.jp>
2022-08-29 08:47:07 +0900

    Delete owner_simple directory

--- owner_simple/target/rls/debug/deps/owner_simple-d7645226b6116a64.d
+++
@@ -1,5 +0,0 @@
-c:\Users\cax68\Documents\VSCODE\Rust\owner_simple\target\rls\debug\deps\owner_simple-d7645226b6116a64.rmeta: src\main.rs
-
-c:\Users\cax68\Documents\VSCODE\Rust\owner_simple\target\rls\debug\deps\owner_simple-d7645226b6116a64.d: src\main.rs
-
-src\main.rs:
